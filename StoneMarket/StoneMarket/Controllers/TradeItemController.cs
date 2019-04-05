4d3b44a6a305cd8c5495b867f763e99e17577c6a
Serhii Tyshchenko <stysh@softserveinc.com>
2019-04-05 12:38:14 +0300

    Add new controller

---
+++ StoneMarket/StoneMarket/Controllers/TradeItemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoneMarket.Controllers
+{
+    public class TradeItemController : Controller
+    {
+        // GET: TradeItem
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
