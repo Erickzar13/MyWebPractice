264bf8267fddad8989a52244942d0690b388c01c
Serhii Tyshchenko <stysh@softserveinc.com>
2019-04-05 12:12:42 +0300

    Add two new models

---
+++ StoneMarket/StoneMarket/Models/PurchaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoneMarket.Models
+{
+    public class PurchaseModel
+    {
+        string CustomerId { get; set; }
+        string Address { get; set; }
+        public int TradeItemId { get; set; }
+    }
+}
---
+++ StoneMarket/StoneMarket/Models/TradeItemModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoneMarket.Models
+{
+    public class TradeItemModel
+    {
+        string Type { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        string Price { get; set; }
+    }
+}
